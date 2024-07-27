cf77d6ba21ec330ec9474dcfed2bf79addf4e17b
yuriee220 <y.date220@gmail.com>
2024-07-27 15:48:11 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+
+<body>
+    <p>
+        <?php
+        $vegetable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+        print_r($vegetable_data);
+        ?>
+    </p>
+</body>
+ 
+ </html>
