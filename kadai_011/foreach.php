8400afa904008dc17462b5476eb1cd60ba96a487
yuriee220 <y.date220@gmail.com>
2024-07-28 01:51:12 +0900

    kadai_011をアップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+
+<body>
+    <p>
+        <?php
+        $vegetable_data = ['名前' => '玉ねぎ', '値段' => '200', '産地' => '北海道'];
+        foreach ($vegetable_data as $key => $value) {
+            echo "{$key} : {$value}<br>";
+        }
+        ?>
+    </p>
+</body>
+
+ </html>
