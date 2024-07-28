d4c133fedd634be91a6fb6534ee479cf319dc690
yuriee220 <y.date220@gmail.com>
2024-07-28 14:00:07 +0900

    kadai_sortをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // ここにコードを書いていく
+        $nums = [15, 4, 18, 23, 10];
+
+        function sort_2way($array, $order) {
+            if ($order) {
+                echo '昇順にソートします。<br>';
+                sort($array);
+                foreach ($array as $num) {
+                    echo $num . '<br>';
+                }
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($array);
+                foreach ($array as $num) {
+                    echo $num . '<br>';
+                }
+            }
+        }
+       sort_2way($nums,true);
+       sort_2way($nums,false);
+
+        ?>
+    </p>
+</body>
+
+</html>
